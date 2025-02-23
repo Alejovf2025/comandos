9767b0dd0a73e9ee473b810c73af82ced447ecf9
Alejovf2025 <oscar.velasquez-f@uniminuto.edu.co>
2025-02-23 18:32:52 -0500

    Primer commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Oscar Alejandro Velasquez Fajardo");
+        System.out.println("Ciclismo");
+        System.out.println("Videojuegos");
+        System.out.println("GATO : TRUENO");
+
+    }
+}
